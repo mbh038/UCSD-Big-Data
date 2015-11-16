e7305cc98441ff7088a812e3c98d830f197b3263
unknown <michael.hunt@CAM-7WTC6X1.cornwall.ac.uk>
2015-11-16 17:26:01 +0000

    SJiS

---
+++ Hadoop/SimpleJoinInSpark.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Nov 16 16:49:48 2015
+
+@author: michael.hunt
+"""
+
+
+def split_fileA(line):
+    # split the input line in word and count on the comma
+    word,count=line.split(",")
+    # turn the count to an integer  
+    count=int(count)
+    return (word, count)
+    
+def split_fileB(line):
+    # split the input line into word, date and count_string
+    wordDate,count_string=line.split(",")
+    #count=int(count)
+    date,word=wordDate.split(" ")
+    return (word, date + " " + count_string)
+    
